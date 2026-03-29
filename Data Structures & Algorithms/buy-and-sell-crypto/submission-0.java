2d595e4182b89b0c9492c33f09225324aa2cadc0
Yazael Mercado <86888307+bubusaurio@users.noreply.github.com>
2026-03-29 10:32:23 -0600

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int buy = 0;
+
+
+        for(int i = 0; i<prices.length ; i++){
+            if(prices[i] < prices[buy]){
+                buy = i;
+            }
+
+            maxProfit = Math.max(maxProfit ,prices[i] - prices[buy]);
+        }
+        
+        return maxProfit;
+    }
+}
