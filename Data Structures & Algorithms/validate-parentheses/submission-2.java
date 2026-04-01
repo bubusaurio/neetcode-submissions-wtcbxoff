3f20a4224a0a4efd1ed4b98b7f41f64589a75881
Yazael Mercado <86888307+bubusaurio@users.noreply.github.com>
2026-04-01 13:18:07 -0600

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        if(s.length() == 0){
+            return true;
+        }
+
+        if(s.length() % 2 != 0){
+            return false;
+        }
+
+        if(s.charAt(0) == ')' || s.charAt(0) == ']' || s.charAt(0) == '}') return false;
+
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty()){
+                switch(c){
+                    case ')':
+                        if(stack.pop() != '(') return false;
+                        break;
+                    
+                    case ']':
+                        if(stack.pop() != '[') return false;
+                        break;
+
+                    case '}':
+                        if(stack.pop() != '{') return false;
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+
+            if(c == '(' || c == '[' || c == '{') stack.push(c);
+        }
+
+        return stack.isEmpty();
+    }
+}
