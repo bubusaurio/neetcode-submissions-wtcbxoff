f6115003163baaa5aee35710d7065a4d7c272537
Yazael Mercado <86888307+bubusaurio@users.noreply.github.com>
2026-04-19 12:38:09 -0600

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for(int n : stones){
+            maxHeap.offer(n);
+        }
+
+        while(maxHeap.size() > 1){
+            if(maxHeap.isEmpty()){
+                break;
+            }
+            int stoneX = maxHeap.poll();
+            int stoneY = maxHeap.poll();
+
+            int stoneZ = Math.abs(stoneX - stoneY);
+
+            if(stoneZ != 0){
+                maxHeap.offer(stoneZ);
+            }
+        }
+
+        if(maxHeap.isEmpty()){
+            return 0;
+        }
+        return maxHeap.poll();
+    }
+}
